0609f8e6f583f70a7a181733b05328c375c04d5e
Sinis16 <88800631+Sinis16@users.noreply.github.com>
2022-10-25 04:11:32 +0200

    Add files via upload

---
+++ taller1TypeScript/scripts/serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(num, name, prod, seas, desc, pag, img) {
+        this.num = num;
+        this.name = name;
+        this.prod = prod;
+        this.seas = seas;
+        this.desc = desc;
+        this.pag = pag;
+        this.img = img;
+    }
+    return Serie;
+}());
+export { Serie };
